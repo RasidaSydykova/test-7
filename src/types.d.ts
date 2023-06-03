cb18d09ffb0f15c62689f711c68ee225ba15795d
RasidaSydykova <sydykova.rasida@mail.ru>
2023-06-03 12:02:07 +0600

    Created types.d.ts for interface items

---
+++ src/types.d.ts
@@ -0,0 +1,12 @@
+export interface IItem {
+  id: string;
+  name: string;
+  price: number;
+  image: string;
+}
+
+export interface IItemState {
+  id: string;
+  name: string;
+  count: number;
+}
